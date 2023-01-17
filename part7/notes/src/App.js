227e4985920d95edb9884921b6a8705239105c8c
joshrutta <joshrutta7@gmail.com>
2023-01-16 22:40:19 -0500

    react bootstrap styling

--- part7/notes/src/App.js
+++ part7/notes/src/App.js
@@ -1,4 +1,5 @@
 import { useState } from 'react'
+import { Alert, Button, Form, Navbar, Nav, Table } from 'react-bootstrap'
 
 import {
   Routes,
@@ -30,13 +31,20 @@ const Note = ({ note }) => {
 const Notes = ({ notes }) => (
   <div>
     <h2>Notes</h2>
-    <ul>
-      {notes.map(note =>
-        <li key={note.id}>
-          <Link to={`/notes/${note.id}`}>{note.content}</Link>
-        </li>
-      )}
-    </ul>
+    <Table striped>
+      <tbody>
+        {notes.map(note =>
+          <tr key={note.id}>
+            <td>
+            <Link to={`/notes/${note.id}`}>{note.content}</Link>
+            </td>
+            <td>
+              {note.user}
+            </td>
+          </tr>
+        )}
+      </tbody>
+    </Table>
   </div>
 )
 
@@ -63,15 +71,22 @@ const Login = (props) => {
   return (
     <div>
       <h2>login</h2>
-      <form onSubmit={onSubmit}>
-        <div>
-          username: <input />
-        </div>
-        <div>
-          password: <input type='password' />
-        </div>
-        <button type="submit">login</button>
-      </form>
+      <Form onSubmit={onSubmit}>
+        <Form.Group>
+        <Form.Label>username: </Form.Label>
+        <Form.Control 
+          type='text'
+          name='username'
+        />
+        <Form.Label>password: </Form.Label>
+        <Form.Control
+          name='password'
+        />
+        <Button variant='primary' type='submit'>
+          login
+        </Button>
+        </Form.Group>
+      </Form>
     </div>
   )
 }
@@ -99,12 +114,15 @@ const App = () => {
   ])
 
   const [user, setUser] = useState(null)
+  const [message, setMessage] = useState(null)
 
   const match = useMatch('/notes/:id')
   const note = match ? notes.find(note => note.id === Number(match.params.id)) : null
 
   const login = (user) => {
     setUser(user)
+    setMessage(`welcome ${user}`)
+    setTimeout(() => setMessage(null), 10000)
   }
 
   const padding = {
@@ -112,15 +130,35 @@ const App = () => {
   }
 
   return (
-    <div>
+    <div className='container'>
+      {(message && 
+        <Alert variant='success'>
+          {message}
+        </Alert>
+        )}
         <div>
-          <Link style={padding} to="/">home</Link>
-          <Link style={padding} to="/notes">notes</Link>
-          <Link style={padding} to="/users">users</Link>
-          {user
-            ? <em>{user} logged in</em>
-            : <Link style={padding} to="/login">login</Link>
-          }
+        <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
+          <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+          <Navbar.Collapse id="responsive-navbar-nav">
+            <Nav className="me-auto">
+              <Nav.Link href="#" as="span">
+                <Link style={padding} to="/">home</Link>
+              </Nav.Link>
+              <Nav.Link href="#" as="span">
+                <Link style={padding} to="/notes">notes</Link>
+              </Nav.Link>
+              <Nav.Link href="#" as="span">
+                <Link style={padding} to="/users">users</Link>
+              </Nav.Link>
+              <Nav.Link href="#" as="span">
+                {user
+                  ? <em style={padding}>{user} logged in</em>
+                  : <Link style={padding} to="/login">login</Link>
+                }
+              </Nav.Link>
+            </Nav>
+          </Navbar.Collapse>
+        </Navbar>
         </div>
 
 
